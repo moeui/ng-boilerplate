da9126a09b9c7b837e83390db29f96f188129d1a
nnn <zjx1247225@163.com>
2021-10-01 21:38:15 +0800

    init

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HeaderComponent } from '../../component/header/header.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
